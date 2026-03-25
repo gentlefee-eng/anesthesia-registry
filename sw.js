269911cdc7cf3e176b1bee9c97eefd1acba0da96
gentlefee-eng <gentle.fee@gmail.com>
2026-03-25 21:06:03 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,16 @@
-const CACHE_NAME = 'anesthesia-registry-iframe-shell-v1';
+const CACHE_NAME = 'anesthesia-registry-v1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-
-  if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return resp;
-      }).catch(() => caches.match('./index.html')))
-    );
-  }
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request).catch(() => caches.match('./index.html'))));
 });
